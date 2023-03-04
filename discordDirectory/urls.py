f52a17d4d8d431ab1278d87d6569a940d042a8e2
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-03-04 15:18:39 +0000

    feature: signup and login user

--- discordDirectory/urls.py
+++ discordDirectory/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 # All possible urls from the domain name (can redirect to other urls in applications)
 urlpatterns = [
     path('bots/', include('bots.urls')),
     path('admin/', admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
