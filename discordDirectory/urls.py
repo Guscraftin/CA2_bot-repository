96031240830cbf8683119f50364e129838d92594
Guscraftin <97472565+Guscraftin@users.noreply.github.com>
2023-03-10 11:02:11 +0000

    remove: home page

--- discordDirectory/urls.py
+++ discordDirectory/urls.py
@@ -4,9 +4,10 @@ from django.views.generic.base import TemplateView
 
 # All possible urls from the domain name (can redirect to other urls in applications)
 urlpatterns = [
-    path('bots/', include('bots.urls')),
+    path('', include('bots.urls')),  # Replace by: path('bots', include('bots.urls')),
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    # For the future, currently it is not really useful
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
